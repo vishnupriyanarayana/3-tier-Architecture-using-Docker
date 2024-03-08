949f6b6825d30c0fda2e8d065524e0c6ef87eaea
vishnupriyanarayana <113781108+vishnupriyanarayana@users.noreply.github.com>
2024-03-08 12:20:50 +0530

    Update backend.py

--- backend-flask/backend.py
+++ backend-flask/backend.py
@@ -5,7 +5,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST']='172.22.0.2'
+app.config['MYSQL_HOST']='10.0.0.5'
 app.config['MYSQL_USER']='root'
 app.config["MYSQL_PASSWORD"]=''
 app.config["MYSQL_DB"]='flaskapp'
